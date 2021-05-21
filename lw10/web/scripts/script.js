e40b0f80664445729e1121a265052c1df61b6c28
Swizi <denis.bosiy@gmail.com>
2021-05-21 16:41:27 +0300

    update lw10( add named functions and delete nesting )

--- lw10/web/scripts/script.js
+++ lw10/web/scripts/script.js
@@ -1,3 +1,27 @@
+const XHTTPSTATUS = {
+  Unsent: 0,
+  Opened: 1,
+  HeadersReceived: 2,
+  Loading: 3,
+  Done: 4,
+};
+
+function getFormData(form) {
+  const nameInput = form.getElementsByClassName('name')[0];
+  const emailInput = form.getElementsByClassName('email')[0];
+  const subjectInput = form.getElementsByClassName('subject')[0];
+  const messageInput = form.getElementsByClassName('message')[0];
+
+  const formData = {
+    name: nameInput.value,
+    email: emailInput.value,
+    subject: subjectInput.value,
+    message: messageInput.value,
+  };
+
+  return formData;
+}
+
 function showInputError(input, hint, error) {
   if (error !== undefined) {
     input.classList.add('form__input-text--error');
@@ -10,57 +34,69 @@ function showInputError(input, hint, error) {
   }
 }
 
-function sendRequest() {
-  const nameInput = document.getElementById('name');
-  const emailInput = document.getElementById('email');
-  const subjectInput = document.getElementById('subject');
-  const messageInput = document.getElementById('message');
+function updateFormInterface(form, response) {
+  const nameInput = form.getElementsByClassName('name')[0];
+  const emailInput = form.getElementsByClassName('email')[0];
+  const subjectInput = form.getElementsByClassName('subject')[0];
+  const messageInput = form.getElementsByClassName('message')[0];
 
-  const nameHint = document.getElementById('name-hint');
-  const emailHint = document.getElementById('email-hint');
-  const subjectHint = document.getElementById('subject-hint');
-  const messageHint = document.getElementById('message-hint');
-  const successBox = document.getElementById('success-box');
+  const nameHint = form.getElementsByClassName('name-hint')[0];
+  const emailHint = form.getElementsByClassName('email-hint')[0];
+  const subjectHint = form.getElementsByClassName('subject-hint')[0];
+  const messageHint = form.getElementsByClassName('message-hint')[0];
+  const successBox = form.getElementsByClassName('success-box')[0];
 
+  const nameError = response['errors']['name_msg'];
+  const emailError = response['errors']['email_msg'];
+  const subjectError = response['errors']['subject_msg'];
+  const messageError = response['errors']['message_msg'];
+
+  showInputError(nameInput, nameHint, nameError);
+  showInputError(emailInput, emailHint, emailError);
+  showInputError(subjectInput, subjectHint, subjectError);
+  showInputError(messageInput, messageHint, messageError);
+
+  if (response['is_success']) {
+    successBox.classList.remove('hide-out');
+    setTimeout(() => {
+      successBox.classList.add('hide-out');
+    }, 3000);
+  } else {
+    successBox.classList.add('hide-out');
+  }
+}
+
+function processFormRequest() {
+  const readyState = this.readyState;
+  const httpStatus = this.status;
+
+  if (readyState === XHTTPSTATUS.Done && httpStatus === 200) {
+    // Response
+    const response = JSON.parse(this.response);
+    updateFormInterface(form, response);
+  }
+  if (readyState === XHTTPSTATUS.Done && httpStatus >= 400) {
+    alert('Ошибка сервера');
+  }
+}
+
+function sendRequest(form) {
   const xhttp = new XMLHttpRequest();
   xhttp.open('POST', '../src/save_feedback.php', true);
   xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-  xhttp.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      // Response
-      const response = JSON.parse(this.response);
 
-      const nameError = response['errors']['name_msg'];
-      const emailError = response['errors']['email_msg'];
-      const subjectError = response['errors']['subject_msg'];
-      const messageError = response['errors']['message_msg'];
+  xhttp.onreadystatechange = processFormRequest;
 
-      showInputError(nameInput, nameHint, nameError);
-      showInputError(emailInput, emailHint, emailError);
-      showInputError(subjectInput, subjectHint, subjectError);
-      showInputError(messageInput, messageHint, messageError);
-
-      if (response['is_success']) {
-        successBox.classList.remove('hide-out');
-        setTimeout(() => {
-          successBox.classList.add('hide-out');
-        }, 3000);
-      } else {
-        successBox.classList.add('hide-out');
-      }
-    }
-    if (this.readyState === 4 && this.status >= 400) {
-      alert('Ошибка сервера');
-    }
-  };
-
-  const formData = {
-    name: nameInput.value,
-    email: emailInput.value,
-    subject: subjectInput.value,
-    message: messageInput.value,
-  };
-
-  const requestText = 'form_data=' + JSON.stringify(formData);
+  const requestText = 'form_data=' + JSON.stringify(getFormData(form));
   xhttp.send(requestText);
 }
+
+function run() {
+  const form = document.getElementById("form");
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    sendRequest(form);
+  });
+}
+
+window.onload = run;
